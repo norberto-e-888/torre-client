781e5c1a84be459eb4dfdcbc5255c73c0fbea595
Norberto Cáceres <norberto.e.888@outlook.es>
2021-03-02 00:09:06 -0500

    Handle case where the test has already been taken

--- src/personality-test/pages/TakeTestPage.tsx
+++ src/personality-test/pages/TakeTestPage.tsx
@@ -5,10 +5,12 @@ import axios from 'axios';
 import Page from '../../lib/components/Page';
 import Test from '../components/Test';
 import { Heading } from '@chakra-ui/react';
+import { useAuth } from '../../auth/lib/AuthProvider';
 
 const TakeTestPage: React.FC = () => {
 	const params = useParams() as any;
 	const history = useHistory();
+	const { user } = useAuth();
 	const { data: test, isLoading } = useQuery(
 		['isTestAssignedToUser', params.testId],
 		async () => {
@@ -24,10 +26,65 @@ const TakeTestPage: React.FC = () => {
 		}
 	);
 
+	const testId = test?._id;
+	const userId = user?.id;
+	const {
+		data: wasItAlreadyTaken,
+		isLoading: isLoadingWasTestAlreadyTaken,
+	} = useQuery(
+		['hasUserTakenTest', testId],
+		async () => {
+			try {
+				const response = await axios.get(
+					`/personality-test/${test._id}/has-it-been-taken-by-me`
+				);
+
+				return response.data;
+			} catch (error) {
+				history.replace('/assignments');
+			}
+		},
+		{ enabled: !!testId }
+	);
+
+	const { data: results } = useQuery(
+		['results', testId, userId],
+		async () => {
+			try {
+				const response = await axios.get(
+					`/personality-test-result/get-user-results/${testId}/${userId}`
+				);
+
+				return response.data;
+			} catch (error) {
+				return null;
+			}
+		},
+		{
+			enabled: !!testId && !!userId,
+		}
+	);
+
 	return (
 		<Page>
 			<Heading fontWeight="medium">{test?.alias}</Heading>
-			{!isLoading && test && <Test test={test} />}
+			{!isLoading &&
+				!isLoadingWasTestAlreadyTaken &&
+				test &&
+				(!wasItAlreadyTaken ? (
+					<Test test={test} />
+				) : (
+					results && (
+						<a
+							target="_blank"
+							href={`https://www.16personalities.com/${results.personalityType}-personality`}
+							rel="noreferrer"
+						>
+							Personality Type:{' '}
+							<strong>{results.personalityType.toUpperCase()}</strong>
+						</a>
+					)
+				))}
 		</Page>
 	);
 };
